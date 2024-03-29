5fa4799ae15469ecf5838e86cad74754cd072785
sybaek <sybaek@crepass.com>
2024-03-29 16:17:11 +0900

    크레파스

---
+++ js/crepass.js
@@ -0,0 +1,92 @@
+
+const texts={
+    "en": {
+        "downloadGuide": "Do you not have the PPC BANK credit evaluation app on your phone?<br>Click the button now to download the PPC BANK app.",
+        "installGuide": "PPC BANK Credit Evaluation APP Installation Guide",
+        "btn_app_download": "APP DOWNLOAD",
+        "header-title":"App Download Guide",
+        "guide-1":"After downloading, open the notification list at the top of your phone and run the installation file (apk).",
+        "guide-2":"If you see a prompt for 'Install unknown apps,' please change it to Allow. For instructions on how to change this setting, please refer to the guide below on phone environment settings.",
+        "guide-3":"In the app installation screen, press [Install], and after the installation is complete, press [Open] or [Done].",
+        "guide-4":"Depending on the specifications of your phone, it may be difficult to use the PPC BANK Alternative Credit Evaluation APP.",
+        "crepass-guide-setup-title":"Guide to Phone Environment Settings",
+        "setup-title1":"1. Phone Settings",
+        "setup-content1":"From your phone's settings menu, select 'Security & Privacy'.<br><br>Depending on the OS version, it may be located in Settings > Applications.",
+        "setup-title2":"2. Other Security Settings",
+        "setup-content2":"Select 'Other Security Settings'.",
+        "setup-title3":"3. Install Unknown Apps",
+        "setup-content3":"Select 'Install Unknown Apps'.",
+        "setup-title4":"4. Allow Settings",
+        "setup-content4":"Click on 'My Files' to change the setting value to ON.<br><br>PPC Bank provides only safe apps.",
+    
+    },
+    "km": {
+        "downloadGuide": "តើអ្នកមិនមានកម្មវិធីវាយតម្លៃឥណទានរបស់ PPC BANK នៅលើទូរស័ព្ទរបស់អ្នកទេ?<br>ចុចប៊ូតុងឥឡូវនេះដើម្បីទាញយកកម្មវិធី PPC BANK។",
+        "installGuide": "ការណែនាំការដំឡើងកម្មវិធីវាយតម្លៃឥណទាន PPC BANK",
+        "btn_app_download": "ទាញយកកម្មវិធី",
+        "header-title":"ការណែនាំទាញយកកម្មវិធី",
+        "guide-1":"បន្ទាប់ពីទាញយករួចហើយ សូមបើកបញ្ជីការជូនដំណឹងនៅផ្នែកខាងលើនៃទូរស័ព្ទរបស់អ្នក ហើយដំណើរការឯកសារដំឡើង (apk)។",
+        "guide-2":"ប្រសិនបើអ្នកឃើញការណែនាំសម្រាប់ 'ការ​ដោះ​ស្រាយ​កម្មវិធី​មិន​ស្គាល់​អញ្ញាត' សូមប្ដូរវាទៅជា អនុញ្ញាត។ សម្រាប់ការណែនាំពីរបៀបផ្លាស់ប្ដូរការកំណត់នេះ សូមយោងទៅកាន់ការណែនាំខាងក្រោមនៅលើការកំណត់បរិស្ថានទូរស័ព្ទ។",
+        "guide-3":"នៅក្នុងអេក្រង់ដំឡើងកម្មវិធី ចុច [ដំឡើង] ហើយបន្ទាប់ពីដំឡើងបានបញ្ចប់ ចុច [បើក] ឬ [រួចរាល់]។",
+        "guide-4":"ដោយផ្អែកលើលក្ខណៈប៉ុន្មាននៃទូរស័ព្ទរបស់អ្នក អាចមានបញ្ហាក្នុងការប្រើកម្មវិធី PPC BANK កាលណាបាន។",
+        "crepass-guide-setup-title":"ណែនាំអំពីការកំណត់របស់ទូរស័ព្ទ",
+        "setup-title1":"1. ការកំណត់ទូរស័ព្ទ",
+        "setup-content1":"សូមជ្រើសរើស 'សុវត្ថិភាព និងទីផ្សារឯកជន' ពីនៅក្នុងម៉ូឌុលកម្មវិធីរបស់ទូរស័ព្ទ។<br><br>Depending on the OS version, it may be located in Settings > Applications.",
+        "setup-title2":"2. ការកំណត់សុវត្ថិភាពផ្សេងៗ",
+        "setup-content2":"ជ្រើសរើស 'ការកំណត់សុវត្ថិភាពផ្សេងៗ'។",
+        "setup-title3":"3. ដោយសារជាមួយនឹងការដំឡើងកម្មវិធីមិនស្គាល់។",
+        "setup-content3":"ជ្រើសរើស 'ដំឡើងកម្មវិធីមិនស្គាល់'។",
+        "setup-title4":"4. អនុញ្ញាតឱ្យការកំណត់",
+        "setup-content4":"ចុចលើ 'ឯកសាររបស់ខ្ញុំ' ដើម្បីផ្លាស់ប្ដូរតម្លៃការកំណត់ទៅស្តង់ដារ។<br><br>PPC Bank ផ្ដល់ជា​​មិនប្រហារជាងកម្មវិធីសុវត្ថិភាព។",
+    
+    },
+    "ko": {
+        "downloadGuide": "휴대폰에 PPC BANK 대안신용평가 어플이 없나요?<br>지금 버튼을 눌러 PPC BANK 어플을<br>다운로드 하세요.",
+        "installGuide": "PPC BANK 대안신용평가 APP 설치 안내",
+        "btn_app_download":"앱 다운로드",
+        "header-title":"앱 다운로드 안내",
+        "guide-1":"다운로드 후 휴대폰 상단의 알림 목록을 열어 설치파일(apk)을 실행합니다",
+        "guide-2":"‘출처를 알수 없는 앱 설치’ 안내가 나오면 허용으로 변경해 주세요. 설정에서 허용으로 변경하는 방법은 아래 휴대폰 환경 설정 안내를 참고해 주세요.",
+        "guide-3":"앱 설치 화면에서 [설치]를 누른 후, 설치가 완료되면 [열기] 또는 [완료]를 눌러 주세요.",
+        "guide-4":"휴대폰의 사양에 따라 PPC BANK 대안신용평가 APP 이용이 어려운 경우도 있습니다.",
+        "crepass-guide-setup-title":"휴대폰 환경 설정 안내",
+        "setup-title1":"1. 휴대폰 설정",
+        "setup-content1":"휴대폰의 설정 메뉴에서 <br>'보안 및 개인정보 보호'를 선택하세요.<br><br>OS버전에 따라 설정 > 응용 프로그램 항목에 있을 수 있습니다.",
+        "setup-title2":"2. 기타 보안 설정",
+        "setup-content2":"'기타 보안 설정'을 선택합니다.",
+        "setup-title3":"3. 출처를 알 수 없는 앱 설치",
+        "setup-content3":"'출처를 알 수 없는 앱 설치'를 선택합니다.",
+        "setup-title4":"4. 설정 허용",
+        "setup-content4":"'내 파일'을 클릭하여 설정값을<br>ON으로 변경합니다.<br><br>PPC Bank는 안전한 앱만 제공합니다.",
+    
+    }
+};
+document.getElementById('languageSelect').addEventListener('change', function() {
+    updateLanguage(this.value);
+});
+
+function updateLanguage(language) {
+    if(language=="language"){
+        return
+    }
+    // 예를 들어, 'downloadGuide'와 'installGuide' 텍스트 업데이트
+    document.querySelector('.header-title').innerHTML = texts[language]['header-title'];
+    document.querySelector('.btn-download').innerHTML = texts[language]['btn_app_download'];
+    document.querySelector('.crepass-download-info-txt').innerHTML = texts[language]['downloadGuide'];
+    document.querySelector('.crepass-download-guide-title').innerHTML = texts[language]['installGuide'];
+    document.querySelector('.guide-1').innerHTML = texts[language]['guide-1'];
+    document.querySelector('.guide-2').innerHTML = texts[language]['guide-2'];
+    document.querySelector('.guide-3').innerHTML = texts[language]['guide-3'];
+    document.querySelector('.guide-4').innerHTML = texts[language]['guide-4'];
+    document.querySelector('.crepass-guide-setup-title').innerHTML = texts[language]['crepass-guide-setup-title'];
+    document.querySelector('.setup-title1').innerHTML = texts[language]['setup-title1'];
+    document.querySelector('.setup-content1').innerHTML = texts[language]['setup-content1'];
+    document.querySelector('.setup-title2').innerHTML = texts[language]['setup-title2'];
+    document.querySelector('.setup-content2').innerHTML = texts[language]['setup-content2'];
+    document.querySelector('.setup-title3').innerHTML = texts[language]['setup-title3'];
+    document.querySelector('.setup-content3').innerHTML = texts[language]['setup-content3'];
+    document.querySelector('.setup-title4').innerHTML = texts[language]['setup-title4'];
+    document.querySelector('.setup-content4').innerHTML = texts[language]['setup-content4'];
+
+    // 여기에 다른 텍스트 요소에 대한 업데이트 코드를 추가하세요.
+}
